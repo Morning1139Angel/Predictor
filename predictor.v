06dda7da2f9bf80b1e928181d789c01260472b8a
amir khazama <amirkhazama1139@gmail.com>
2023-06-01 10:19:09 +0330

    saturation

--- predictor.v
+++ predictor.v
@@ -1,5 +1,22 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
+    reg[1:0] state;
 
-// Your code
+    initial begin
+        state = 2'b00;
+    end
 
+    always @(posedge clk) begin
+        if (result) begin
+            prediction = state[1];
+        end
+
+        if (request ) begin
+            if (taken && state != 2'b11) begin
+                state = state + 1 ;
+            end
+            if (!taken && state != 2'b00) begin
+                state = state - 1 ;
+            end
+        end
+    end
 endmodule
